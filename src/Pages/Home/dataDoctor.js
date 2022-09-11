8d93a7c3376eaece5c44c6211bd977a79c299785
jesselynmu <mujesselyn@gmail.com>
2022-09-11 22:59:54 +0700

    update

--- src/Pages/Home/dataDoctor.js
+++ src/Pages/Home/dataDoctor.js
@@ -1,32 +1,70 @@
-import Group1 from '../../Assets/images/Group 1.svg';
+import Doctor1 from '../../Assets/images/Group 1.svg';
+import Doctor2 from '../../Assets/images/Group 3.svg';
+import Doctor3 from '../../Assets/images/Group 4.svg';
+import Doctor4 from '../../Assets/images/Group 5.svg';
+import Doctor5 from '../../Assets/images/Group 6.svg';
+import Doctor6 from '../../Assets/images/Group 7.svg';
+import Doctor7 from '../../Assets/images/Group 8.svg';
+import Doctor8 from '../../Assets/images/Group 9.svg';
 
 export const dataPsychiatrist = [
     {
-        img: Group1,
+        img: Doctor1,
         name: "Dr. Agus Utomo Sp.KJ",
         experience: "1 years",
         contact: "+62 86789085432",
         rate: "90%"
     },
     {
-        img: Group1,
-        name: "Dr. Jesslyn Callista Sp.KJ",
-        experience: "1 years",
-        contact: "+62 86789085432",
-        rate: "90%"
+        img: Doctor2,
+        name: "Dr. Jane Abigail Sp.KJ",
+        experience: "5 years",
+        contact: "+62 83154763040",
+        rate: "98%"
     },
     {
-        img: Group1,
+        img: Doctor3,
         name: "Dr. Sezara Athallah Sp.KJ",
-        experience: "1 years",
-        contact: "+62 86789085432",
-        rate: "90%"
+        experience: "2 years",
+        contact: "+62 81769676836",
+        rate: "96%"
     },
     {
-        img: Group1,
+        img: Doctor4,
         name: "Dr. Clara Mumtaz Sp.KJ",
+        experience: "3 years",
+        contact: "+62 82168203099",
+        rate: "100%"
+    },
+]
+
+export const dataPsychologist = [
+    {
+        img: Doctor5,
+        name: "Adhara Eugenia",
         experience: "1 years",
-        contact: "+62 86789085432",
-        rate: "90%"
+        contact: "+62 86145535467",
+        rate: "95%"
+    },
+    {
+        img: Doctor6,
+        name: "Jimmie Javier",
+        experience: "4 years",
+        contact: "+62 83187903702 ",
+        rate: "97%"
+    },
+    {
+        img: Doctor7,
+        name: "Euanthe Alia",
+        experience: "1 years",
+        contact: "+62 82185891714",
+        rate: "100%"
+    },
+    {
+        img: Doctor8,
+        name: "Zoran Rufinus",
+        experience: "2 years",
+        contact: "+62 80351439806",
+        rate: "99%"
     },
 ]
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -6,8 +6,10 @@ import imgHand from '../../Assets/images/3.svg';
 import flag from '../../Assets/images/flag.svg';
 import heart from '../../Assets/images/heart-pulse.svg';
 import users from '../../Assets/images/users.svg';
-import Group1 from '../../Assets/images/Group 1.svg';
-import { dataPsychiatrist } from './dataDoctor.js'
+import { dataPsychiatrist } from './dataDoctor.js';
+import { dataPsychologist } from './dataDoctor.js';
+import phone from '../../Assets/images/phone.svg';
+import smile from '../../Assets/images/smile.svg';
 import '../../Assets/css/styles.css';
 export default function Home() {
     return (
@@ -108,6 +110,7 @@ export default function Home() {
                     </Grid>
                 </Grid>
             </Grid>
+
             <Grid container className='pl-20 pr-20 h-fit mb-20'>
                 <Grid item lg={12} xl={12}>
                     <p className="text-left pl-10 mt-20 text-3xl font-extrabold text-blueCustom">Get Your Best  Psychiatrist or Psychologist</p>
@@ -146,6 +149,91 @@ export default function Home() {
                                 </Grid>
                             ))
                         }
+                    </Grid>
+                </Grid>
+
+                <Grid item lg={12} xl={12}>
+                    <p className="text-left pl-10 my-10 text-xl font-bold text-blueCustom">Psychiatrist</p>
+                </Grid>
+                <Grid item lg={12} xl={12}>
+                    <Grid container spacing={5}>
+                        {
+                            dataPsychologist?.map((data, index) => (
+                                <Grid item lg={3} xl={3} key={index}>
+                                    <div className="w-5/6 mx-auto">
+                                        <div className="bg-blueSoft h-[380px] p-3 rounded-3xl">
+                                            <div className="flex flex-col space-y-4 p-3">
+                                                <div className="w-full">
+                                                    <center>
+                                                        <img src={data.img} />
+                                                    </center>
+                                                </div>
+                                                <div className="w-full">
+                                                    <p className="text-black font-bold">{data.name}</p>
+                                                </div>
+                                                <div className="w-full">
+                                                    <p className="text-black text-sm text-left">{data.experience}</p>
+                                                </div>
+                                                <div className="w-full">
+                                                    <div className="flex flex-row justify-between items-center">
+                                                        <p className="text-black text-sm text-left">{data.contact}</p>
+                                                        <p className="text-black text-sm text-left">{data.rate}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Grid>
+                            ))
+                        }
+                    </Grid>
+                </Grid>
+
+                <Grid container className='pl-10 pr-10 h-fit mb-20'>
+                    <Grid item lg={12} xl={12}>
+                        <p className="my-20 text-3xl font-extrabold text-blueCustom">We Care About You</p>
+                    </Grid>
+                    <Grid item lg={12} xl={12}>
+                        <Grid container spacing={5}>
+                            <Grid item lg={4} xl={4}>
+                                <div className="w-3/2 mx-auto">
+                                    <div className="bg-blueSoft h-[290px] p-3 rounded-3xl">
+                                        <div className="flex flex-col space-y-4 p-3">
+                                            <div className="w-full">
+                                                <p className="text-black font-bold text-xl pb-5">Contact Us For Report</p>
+                                            </div>
+                                            <div className="w-full pb-5">
+                                                <center>
+                                                    <img src={phone} />
+                                                </center>
+                                            </div>
+                                            <div className="w-full">
+                                                <button className="btn btn--color float-center">Contact</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Grid>
+                            <Grid item lg={4} xl={4}>
+                                <div className="w-3/2 mx-auto">
+                                    <div className="bg-blueSoft h-[290px] p-3 rounded-3xl">
+                                        <div className="flex flex-col space-y-4 p-3">
+                                            <div className="w-full">
+                                                <p className="text-black font-bold text-xl pb-5">Join Community</p>
+                                            </div>
+                                            <div className="w-full pb-5">
+                                                <center>
+                                                    <img src={smile} />
+                                                </center>
+                                            </div>
+                                            <div className="w-full">
+                                                <button className="btn btn--color float-center">Join</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
