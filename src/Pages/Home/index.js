65059d5397727dfdca70c985a68522aa93ff0f95
panxzzlyn <panxzzlyn@gmail.com>
2022-09-11 11:54:32 +0700

    update home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react'
-
-
+import { Link, useLocation, NavLink } from "react-router-dom";
+import { Grid } from '@mui/material';
+import imgHand from '../../Assets/images/3.svg'
 export default function Home() {
     return (
-        <p>Test home</p>
+        <Grid container className='mt-20 pl-20 pr-20'>
+            <Grid item lg={8}>
+                <Grid container className='mt-20'>
+                    <Grid item lg={12}>
+                        <p className="float-left">Because Your Life is Precious,</p>
+                    </Grid>
+                    <Grid item lg={12}>
+                        <p className="float-left">We Are There For You</p>
+                    </Grid>
+                    <Grid item lg={12}>
+                        <p className="float-left">Nothing is more valuable than your life, therefore if you need help contact us immediately.</p>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item lg={4}>
+                <img src={imgHand} />
+            </Grid>
+        </Grid>
     )
 }
