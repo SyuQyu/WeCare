5ab14e10017c1d2af28fd8ece3b963178d4e9d84
panxzzlyn <panxzzlyn@gmail.com>
2022-09-12 22:04:37 +0700

    update responsive

--- src/Pages/Components/header.js
+++ src/Pages/Components/header.js
@@ -7,23 +7,23 @@ export default function Header(props) {
     const split = location.pathname.split("/")
     return (
         <Grid container className="p-5">
-            <Grid item lg={8}>
+            <Grid item xl={8} lg={8} md={8} sm={8} xs={6}>
                 <Grid container spacing={2}>
-                    <Grid item lg={12}>
-                            <p className="ml-24 justify-center text-xl font-poppins float-left font-bold text-blueCustom">weCare</p>
+                    <Grid item xl={12} lg={12} md={12}>
+                            <p className="md:ml-24 sm:ml-5 justify-center text-xl font-poppins float-left font-bold text-blueCustom">weCare</p>
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid item lg={4}>
+            <Grid item xl={4} lg={4} md={4} sm={4} xs={6}>
                 <Grid container spacing={2}>
-                    <Grid item lg={4}>
+                    <Grid item xl={4} lg={4} md={4} sm={4}>
                         {/* <NavLink to="About" className={clsx('pt-1 text-base font-poppins float-left font-semibold', split[1] === 'About' ? "text-blueCustom" : "text-disabledColor")}>About Us</NavLink> */}
                     </Grid>
-                    <Grid item lg={4}>
+                    <Grid item xl={4} lg={4} md={4} sm={4}>
                     <NavLink to="Home" className={clsx('pt-1 text-base font-poppins float-left font-bold hover:text-blueCustom', split[1] === 'Home' ? "text-blueCustom" : "text-disabledColor")}>Home</NavLink>
                         {/* <NavLink to="Care" className={clsx('pt-1 text-base font-poppins float-left font-semibold', split[1] === 'Care' ? "text-blueCustom" : "text-disabledColor")}>Care</NavLink> */}
                     </Grid>
-                    <Grid item lg={4}>
+                    <Grid item xl={4} lg={4} md={4} sm={4}>
                         <NavLink to="Consult" className={clsx('pt-1 text-base font-poppins float-left font-bold hover:text-blueCustom', split[1] === 'Consult' ? "text-blueCustom" : "text-disabledColor")}>Consult</NavLink>
                     </Grid>
                 </Grid>
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -20,74 +20,74 @@ export default function Home(props) {
     };
     return (
         <>
-            <Grid container className='mt-20 mb-20 pl-20 pr-20 h-fit'>
-                <Grid item lg={8}>
-                    <Grid container className='mt-20 pl-8'>
-                        <Grid item lg={12} xl={12}>
-                            <p className="text-left font-poppins text-2xl font-medium text-blueCustom mb-4">Karena Hidupmu Sangat Berharga,</p>
+            <Grid container className='md:mt-20 sm:mt-5 md:mb-20 sm:mb-5 md:px-20 sm:px-5 h-fit'>
+                <Grid item lg={8} xl={8} md={8} sm={8} xs={12}>
+                    <Grid container className='md:mt-20 sm:mt-5 md:pl-8'>
+                        <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
+                            <p className="text-left font-poppins md:text-2xl sm:font-medium text-blueCustom mb-4">Karena Hidupmu Sangat Berharga,</p>
                         </Grid>
-                        <Grid item lg={12} xl={12}>
-                            <p className="text-left font-poppins text-5xl font-semibold text-blueCustom mb-4">Kami Hadir Untukmu</p>
+                        <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
+                            <p className="text-left font-poppins md:text-5xl font-semibold text-blueCustom mb-4">Kami Hadir Untukmu</p>
                         </Grid>
-                        <Grid item lg={12} xl={12}>
+                        <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
                             <div className='w-2/3'>
-                                <p className="text-left font-poppins text-xl text-blueMedium mb-8">Tidak ada yang lebih berharga dari hidupmu, jika butuh bantuan kontak kami secepatnya.</p>
+                                <p className="text-left font-poppins md:text-xl text-blueMedium mb-8">Tidak ada yang lebih berharga dari hidupmu, jika butuh bantuan kontak kami secepatnya.</p>
                             </div>
                         </Grid>
-                        <Grid item lg={12} xl={12}>
-                            <div className='grid gap-4 grid-cols-2 w-1/3'>
+                        <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
+                            <div className='grid gap-4 grid-cols-2 md:w-1/3 xs:w-2/3'>
                                 <button onClick={() => scrollDown(aboutSection)} className="btn btn--color float-left">Consult</button>
                                 <button onClick={() => scrollDown(careSection)} className="btn btn--color float-left">Care</button>
                             </div>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item lg={4}>
+                <Grid item lg={4} xl={4} md={4} sm={4} xs={12}>
                     <center>
-                        <img src={imgHand} />
+                        <img className="md:w-full sm:w-full xs:w-2/3 md:h-full sm:h-full xs:h-2/3 justify-center" src={imgHand} />
                     </center>
                 </Grid>
             </Grid>
-            <Grid container className='pl-20 pr-20 h-fit mb-20' ref={aboutSection}>
-                <Grid item lg={12} xl={12}>
+            <Grid container className='md:px-20 sm:px-5 h-fit md:mb-20 sm:mb-5' ref={aboutSection}>
+                <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
                     <p className="my-20 text-3xl font-extrabold text-blueCustom">Tentang Kami</p>
                 </Grid>
-                <Grid item lg={12} xl={12}>
+                <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
                     <Grid container spacing={5}>
-                        <Grid item lg={4} xl={4}>
+                        <Grid item lg={4} xl={4} md={4} sm={12} xs={12}>
                             <div className="w-2/3 mx-auto">
-                                <div className="bg-blueMedium h-[300px] p-3 rounded-3xl shadow-md">
+                                <div className="bg-blueMedium h-fit p-3 rounded-3xl shadow-md">
                                     <div className="flex flex-col space-y-4 p-3">
                                         <div className="w-full">
                                             <p className="text-white font-bold">Psikiater / Psikolog</p>
                                             <p className="text-white font-bold">Terbaik</p>
                                         </div>
-                                        <div className="w-full">
+                                        <div className="w-full h-fit">
                                             <center>
-                                                <img src={heart} />
+                                                <img className="md:w-full sm:w-full xs:w-2/3 md:h-full sm:h-full xs:h-2/3" src={heart} />
                                             </center>
                                         </div>
-                                        <div className="w-full">
+                                        <div className="w-full h-fit">
                                             <p className="text-white text-sm">Kami menyediakan psikiater/psikolog terbaik untukmu.</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </Grid>
-                        <Grid item lg={4} xl={4}>
+                        <Grid item lg={4} xl={4} md={4} sm={12} xs={12}>
                             <div className="w-2/3 mx-auto">
-                                <div className="bg-blueSoft h-[300px] p-3 rounded-3xl shadow-md">
+                                <div className="bg-blueSoft h-fit p-3 rounded-3xl shadow-md">
                                     <div className="flex flex-col space-y-4 p-3">
                                         <div className="w-full">
                                             <p className="text-black font-bold">Kami Peduli Akan</p>
                                             <p className="text-black font-bold">Hidupmu</p>
                                         </div>
-                                        <div className="w-full">
+                                        <div className="w-full h-fit">
                                             <center>
-                                                <img src={flag} />
+                                                <img className="md:w-full sm:w-full xs:w-2/3 md:h-full sm:h-full xs:h-2/3" src={flag} />
                                             </center>
                                         </div>
-                                        <div className="w-full">
+                                        <div className="w-full h-fit">
                                             <p className="text-black text-sm mt-3">Beritahu kami apabila ada tanda</p>
                                             <p className="text-black text-sm">bunuh diri disekitarmu</p>
                                         </div>
@@ -95,20 +95,20 @@ export default function Home(props) {
                                 </div>
                             </div>
                         </Grid>
-                        <Grid item lg={4} xl={4}>
+                        <Grid item lg={4} xl={4} md={4} sm={12} xs={12}>
                             <div className="w-2/3 mx-auto">
-                                <div className="bg-blueMedium h-[300px] p-3 rounded-3xl shadow-md">
+                                <div className="bg-blueMedium h-fit p-3 rounded-3xl shadow-md">
                                     <div className="flex flex-col space-y-4 p-3">
                                         <div className="w-full">
                                             <p className="text-white font-bold">Kami Ada</p>
                                             <p className="text-white font-bold">Untukmu</p>
                                         </div>
-                                        <div className="w-full h-[100px]">
+                                        <div className="w-full h-fit">
                                             <center>
-                                                <img src={users} />
+                                                <img className="md:w-full sm:w-full xs:w-2/3 md:h-full sm:h-full xs:h-2/3" src={users} />
                                             </center>
                                         </div>
-                                        <div className="w-full h-[100px]">
+                                        <div className="w-full h-fit">
                                             <p className="text-white text-sm mt-3">Join komunitas kami dan temukan</p>
                                             <p className="text-white text-sm">teman terbaik untukmu</p>
                                         </div>
@@ -120,15 +120,15 @@ export default function Home(props) {
                 </Grid>
             </Grid>
 
-            <Grid container className='pl-20 pr-20 h-fit mb-20' ref={careSection}>
+            <Grid container className='md:px-20 sm:px-5 h-fit md:mb-20 sm:mb-5' ref={careSection}>
                 <Grid container className='pl-10 pr-10 h-fit mb-20'>
-                    <Grid item lg={12} xl={12}>
+                    <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
                         <p className="my-20 text-3xl font-extrabold text-blueCustom">Kami Ada Untukmu</p>
                     </Grid>
-                    <Grid item lg={12} xl={12}>
-                        <Grid container spacing={10}>
-                            <Grid item lg={2} xl={2} />
-                            <Grid item lg={4} xl={4}>
+                    <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
+                        <Grid container spacing={5}>
+                            <Grid item lg={2} xl={2} md={2} sm={0} xs={0} />
+                            <Grid item lg={4} xl={4} md={4} sm={12} xs={12}>
                                 <div className="w-full">
                                     <div className="bg-blueSoft h-fit p-3 rounded-3xl shadow-md">
                                         <div className="flex flex-col space-y-4 p-3">
@@ -137,7 +137,7 @@ export default function Home(props) {
                                             </div>
                                             <div className="w-full pb-5">
                                                 <center>
-                                                    <img src={phone} />
+                                                    <img className="md:w-full sm:w-full xs:w-2/3 md:h-full sm:h-full xs:h-2/3" src={phone} />
                                                 </center>
                                             </div>
                                             <div className="w-full">
@@ -146,13 +146,13 @@ export default function Home(props) {
 
                                             </div>
                                             <div className="w-full">
-                                                <button onClick={e => navigate('/contact')} className="btn btn--color float-center w-1/3">Contact</button>
+                                                <button onClick={e => navigate('/contact')} className="btn btn--color float-center md:w-1/3 sm:w-full">Contact</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </Grid>
-                            <Grid item lg={4} xl={4}>
+                            <Grid item lg={4} xl={4} md={4} sm={12} xs={12}>
                                 <div className="w-full">
                                     <div className="bg-blueSoft h-fit p-3 rounded-3xl shadow-md">
                                         <div className="flex flex-col space-y-4 p-3">
@@ -161,14 +161,14 @@ export default function Home(props) {
                                             </div>
                                             <div className="w-full pb-5">
                                                 <center>
-                                                    <img src={smile} />
+                                                    <img className="md:w-full sm:w-full xs:w-2/3 md:h-full sm:h-full xs:h-2/3" src={smile} />
                                                 </center>
                                             </div>
                                             <div className="w-full">
                                                 <p className="text-black text-sm mt-3">Join komunitas kami dan dapatkan pengalaman seru untukmu.</p>
                                             </div>
                                             <div className="w-full">
-                                                <button className="btn btn--color float-center w-1/3">Join</button>
+                                                <button className="btn btn--color float-center md:w-1/3 sm:w-full">Join</button>
                                             </div>
                                         </div>
                                     </div>
