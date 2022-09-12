d79ef677b9279f48cf54fe4856f0758d2537ecdd
panxzzlyn <panxzzlyn@gmail.com>
2022-09-12 20:55:41 +0700

    update footer

--- src/Pages/Components/footer.js
+++ src/Pages/Components/footer.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <footer class="p-4 bg-blueCustom shadow md:flex md:items-center md:justify-between md:p-6">
-            <span class="text-sm sm:text-center text-white">© 2022 <a href="https://flowbite.com/" class="hover:underline">weCare</a>. All Rights Reserved.
+            <span class="text-sm sm:text-center text-white">© 2022 <a href="#" class="hover:underline">weCare</a>. All Rights Reserved.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm text-white sm:mt-0">
                 <li>
